2627083ffc7758ae543348a710846a4d81900c11
HieuColdHead <hnnm05310111@gmail.com>
2024-11-15 05:32:45 +0700

    add_function_searchAlbum

--- MediaApp/Storage/Album/AlbumManage.xaml.cs
+++ MediaApp/Storage/Album/AlbumManage.xaml.cs
@@ -98,5 +98,39 @@ namespace MediaApp
                 AlbumDataGrid.ItemsSource = arr;
             }
         }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchQuery = SearchAlbumTextBox.Text.Trim(); // Lấy từ khóa tìm kiếm từ TextBox
+            if (string.IsNullOrWhiteSpace(searchQuery)) // Nếu không có từ khóa tìm kiếm
+            {
+                System.Windows.MessageBox.Show("Please enter a search term.", "No Search Term", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            // Giả sử _playlistService có phương thức GetPlaylistsByName để tìm Playlist theo tên
+            var filteredAlbum = _albumService.GetAlbumByName(searchQuery);
+            if (filteredAlbum == null || filteredAlbum.Count == 0) // Nếu không tìm thấy Playlist nào
+            {
+                System.Windows.MessageBox.Show($"No playlists found with the keyword '{searchQuery}'.", "No Results", MessageBoxButton.OK, MessageBoxImage.Information);
+                FillData(new List<TbAlbum>()); // Xóa dữ liệu khỏi DataGrid nếu không có kết quả
+            }
+            else
+            {
+                FillData(filteredAlbum); // Chuyển TbPlaylist thành List<TbPlaylist>
+
+            }
+        }
+
+        private void SearchAlbumTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = ((System.Windows.Controls.TextBox)sender).Text.Trim(); // Lấy văn bản từ TextBox            
+        }
+
+        private void FillData(List<TbAlbum> songs)
+        {
+            // Cập nhật UI với danh sách Playlist
+            AlbumDataGrid.ItemsSource = songs;
+        }
     }
 }
